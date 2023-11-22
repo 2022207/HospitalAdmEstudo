6699d7c617cde1840f4ba408fdd0e9c4b3ec6fca
2022207 <2022207@student.cct.ie>
2023-11-22 03:12:58 +0000

    Added dbsetup to main and removed single quotes from  sql statement

--- src/hospitaladmestudo/HospitalAdmEstudo.java
+++ src/hospitaladmestudo/HospitalAdmEstudo.java
@@ -4,6 +4,8 @@
  */
 package hospitaladmestudo;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author castr
@@ -13,7 +15,7 @@ public class HospitalAdmEstudo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         Patient p1 = new Patient("Sam", "15/11/23", "O+");
         Patient p2 = new Patient("Lexie", "27/11/21", "A-");
         
@@ -21,6 +23,12 @@ public class HospitalAdmEstudo {
         System.out.println(p2.getPatientID());
         
         System.out.println(Patient.getCurrentID());
+        if (DatabaseSetup.setupDB()){
+            System.out.println("Database and table created");
+            
+        } else{
+            System.out.println("Oh no! There was a database creation problem...");
+        }
     }
     
 }
